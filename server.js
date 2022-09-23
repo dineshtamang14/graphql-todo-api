d404349eb02c901b10499b9b5609d1795edc1ecd
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-09-23 22:39:32 +0530

    create more query and mutation

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { users, tasks} = require("./constants/index");
+const uuid = require("uuid");
 
 const typeDefs = gql`
     type Query {
         greetings: [String!]
         tasks: [Task!]
+        task(id: ID!): Task 
+        users: [User!]
+        user(id: ID!): User
     } 
+
+    input createtaskInput {
+        name: String!
+        completed: Boolean!
+        userId: ID!
+    }
+
+    input userSignUpInput {
+        name: String!
+        email: String!
+    }
+
+    type Mutation {
+        createTask(input: createtaskInput!): Task
+        userSignUp(input: userSignUpInput!): User 
+    }
 
     type User {
         id: ID!
@@ -25,12 +45,33 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         greetings: () => ["hello", "world"],
-        tasks: () => tasks
+        tasks: () => tasks,
+        task: (_, {id} ) => tasks.find(task => task.id === id),
+        users: () => users,
+        user: (_, {id}) => users.find(user => user.id === id)
+    },
+
+    Mutation: {
+        createTask: (_, {input}) => {
+            const task = {...input, id: uuid.v4()};
+            tasks.push(task);
+            return task;
+        },
+
+        userSignUp: (_, {input}) => {
+            const user = {...input, id: uuid.v4()};
+            users.push(user);
+            return user;
+        }
     },
 
     // field level resolvers
     Task: {
-        user: ({ userId }) => users.find(user => user.id == userId)
+        user: ({ userId }) => users.find(user => user.id == userId),
+    },
+
+    User: {
+        tasks: ({ id }) => tasks.filter(task => task.userId === id)
     }
 };
 
