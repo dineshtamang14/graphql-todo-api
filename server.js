9e816347a85883ed0f35607de4bbe7f0735a08cc
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-09-25 17:05:07 +0530

    handled error using formatError

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ const server = new ApolloServer({
             user: new Dataloader(keys => loaders.user.batchUsers(keys))
         };
         return contextObj;
+    },
+    formatError: (error) => {
+        // console.error(error);
+        return {
+            message: error.message
+        }
     }
 })
 
