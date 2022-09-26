375df0250ad6eeda4a7413a949216b1fe4f2d919
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-09-26 18:18:52 +0530

    created docker-compose file

--- database/util/index.js
+++ database/util/index.js
@@ -6,7 +6,7 @@ module.exports.connection = async () => {
     try {
         // to log all the querys hit by the mongoose
         // mongoose.set('debug', true);
-        await mongoose.connect(process.env.MONGODB_PROD_URL, {
+        await mongoose.connect("mongodb+srv://dinesh:dinesh1997@cluster0.cuuqa.mongodb.net/Todo-DB?retryWrites=true&w=majority" || process.env.MONGODB_PROD_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });   
